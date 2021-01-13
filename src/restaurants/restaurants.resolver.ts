b4a7f08a137dbc2f137bc0d2a86e4300102ee311
MoltenPL85 <bugaev.maxim.85@gmail.com>
2021-01-13 22:48:14 +0500

    Create entity and data mapper demos

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,12 +1,14 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
+import { RestaurantService } from './restaurants.service';
 
 @Resolver(() => Restaurant)
 export class RestaurantResolver {
+  constructor(private readonly restaurantServiceType: RestaurantService) {}
   @Query(() => [Restaurant])
-  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
-    return [];
+  restaurants(): Promise<Restaurant[]> {
+    return this.restaurantServiceType.getAll();
   }
 
   @Mutation(() => Boolean)
