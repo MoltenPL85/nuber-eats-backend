3ac77cb3c52b8aa2321b536550b7b1708ccf79e8
MoltenPL85 <bugaev.maxim.85@gmail.com>
2021-02-02 21:40:47 +0500

    Payments query, mutation and entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -11,6 +11,7 @@ import { InternalServerErrorException } from '@nestjs/common';
 import { IsBoolean, IsEmail, IsEnum, IsString } from 'class-validator';
 import { Restaurant } from '../../restaurants/entities/restaurant.entity';
 import { Order } from '../../orders/entities/order.entity';
+import { Payment } from '../../payments/entities/payment.entity';
 
 export enum UserRole {
   Client = 'Client',
@@ -55,6 +56,10 @@ export class User extends CoreEntity {
   @Field(() => [Order])
   @OneToMany(() => Order, (order) => order.driver)
   orders: Order[];
+
+  @Field(() => [Payment])
+  @OneToMany(() => Payment, (payment) => payment.user, { eager: true })
+  payments: Payment[];
 
   @BeforeInsert()
   @BeforeUpdate()
